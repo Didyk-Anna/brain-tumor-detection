17a78f4390421c0e0a1588194267e1573556b3ee
Didyk-Anna <93837729+Didyk-Anna@users.noreply.github.com>
2024-03-29 21:02:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ import gdown
 from util import visualize, set_background
 
 set_background('./bg.jpg')
+
+@st.cache_resource 
+def load_model():
+    url = "https://drive.google.com/uc?id=1aQmD0IA4rNtTffOOAK758DQjNPfAjP1y"
+    output = "model.pth"
+
+    # Download model file if it doesn't exist
+    if not os.path.exists(output):
+        with st.spinner('Downloading model file...'):
+            gdown.download(url, output)
+    
+    # load model
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/retinanet_R_101_FPN_3x.yaml'))
+    cfg.MODEL.WEIGHTS = 'model.pth'
+    cfg.MODEL.DEVICE = 'cpu'
+
+    predictor = DefaultPredictor(cfg)
+    return predictor
