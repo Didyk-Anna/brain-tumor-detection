871d70fe5d3f607b6f31316163c921bc7f1da826
Didyk-Anna <93837729+Didyk-Anna@users.noreply.github.com>
2024-04-02 00:05:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-#from detectron2.config import get_cfg
-#from detectron2.engine import DefaultPredictor
-#from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+from detectron2 import model_zoo
 from PIL import Image
 import numpy as np
 import os
@@ -47,3 +47,30 @@ file = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
 predictor = load_model()
 
 # load image
+if file:
+    image = Image.open(file).convert('RGB')
+    image_array = np.asarray(image)
+
+    # detect objects
+    outputs = predictor(image_array)
+
+    threshold = 0.5
+
+    # Display predictions
+    preds = outputs["instances"].pred_classes.tolist()
+    scores = outputs["instances"].scores.tolist()
+    bboxes = outputs["instances"].pred_boxes
+
+    bboxes_ = []
+    for j, bbox in enumerate(bboxes):
+        bbox = bbox.tolist()
+
+        score = scores[j]
+        pred = preds[j]
+
+        if score > threshold:
+            x1, y1, x2, y2 = [int(i) for i in bbox]
+            bboxes_.append([x1, y1, x2, y2])
+
+    # visualize
+    visualize(image, bboxes_)
