5ae0b79d5783b50aefbf8b23e7f9a8c00114eb50
Didyk-Anna <93837729+Didyk-Anna@users.noreply.github.com>
2024-04-11 21:01:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
-from detectron2 import model_zoo
+#from detectron2.config import get_cfg
+#from detectron2.engine import DefaultPredictor
+#from detectron2 import model_zoo
 from PIL import Image
 import numpy as np
 import os
@@ -9,7 +9,7 @@ import torch
 from io import BytesIO
 import tempfile
 import gdown
-from util import visualize, set_background
+#from util import visualize, set_background
 
 set_background('./bg.jpg')
 
